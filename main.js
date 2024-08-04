938e7fa64c07fe7c64a86347052d3e58d9442c80
oliverjam <oliverjamesphillips@gmail.com>
2024-08-04 13:50:06 +0100

    Support boosting links as well as forms

--- main.js
+++ main.js
@@ -2,64 +2,106 @@
 
 "use strict";
 
-document.addEventListener("submit", async (event) => {
-	let form = event.target;
-	if (!(form instanceof HTMLFormElement)) return;
-	if (!form.hasAttribute("data-boost")) return;
-	let { confirm, target, select, swap, reset, history, delay } = config(form);
+/**
+ * @param {MouseEvent | SubmitEvent} event
+ */
+async function handle(event) {
+	let el = event.target;
+	if (!(el instanceof HTMLFormElement)) return;
+	if (!(el instanceof HTMLAnchorElement)) return;
+	if (!el.hasAttribute("data-boost")) return;
 
-	// Submit the form ourselves
+	let { confirm, target, select, swap, reset, history, delay } = config(el);
+
 	event.preventDefault();
+
 	if (confirm && window.confirm(confirm) === false) return;
-	let res = await submit(form, event.submitter, history);
+
+	let submitter = event instanceof SubmitEvent ? event.submitter : null;
+	let res = await request(el, submitter, history);
 
 	// Find existing element to update
-	/** @type {HTMLElement | null} */
-	let replacee = document.body;
-	if (target === "this") replacee = form;
-	else if (target?.startsWith("closest"))
-		replacee = form.closest(target.replace("closest ", ""));
-	else if (target) replacee = document.querySelector(target);
-	if (replacee === null) throw new Error(`target ${target} not found`);
-
+	let replacee = find_target(el, target);
+	if (replacee === null) {
+		throw new Error(`target ${target} not found`);
+	}
 	// Find new element to use from response
-	let dom = new DOMParser().parseFromString(await res.text(), "text/html");
-	let replacer = select ? dom.querySelector(select) : dom.body;
-	if (replacer === null) throw new Error(`select ${target} not found`);
+	let replacer = find_selection(await res.text(), select);
+	if (replacer === null) {
+		throw new Error(`select ${select} not found`);
+	}
 
+	// Update the page
 	replacee.setAttribute("data-swapping", "");
+	swap_elements(replacee, replacer, swap);
 	if (delay) await wait(delay);
 	replacee.removeAttribute("data-swapping");
 
-	// Update the page
+	// Reset the form
+	if (reset && "reset" in el) el.reset();
+}
+
+document.addEventListener("click", handle);
+document.addEventListener("submit", handle);
+
+/**
+ * @param {HTMLAnchorElement | HTMLFormElement} el
+ * @param {string | undefined} target
+ */
+function find_target(el, target) {
+	if (!target) return document.body;
+	if (target === "this") return el;
+	if (target.startsWith("closest")) {
+		return el.closest(target.replace("closest ", ""));
+	}
+	return document.querySelector(target);
+}
+
+/**
+ * @param {string} body
+ * @param {string | undefined} select
+ */
+function find_selection(body, select) {
+	let dom = new DOMParser().parseFromString(body, "text/html");
+	if (select) return dom.querySelector(select);
+	return dom.body;
+}
+
+/**
+ * @typedef {"delete" | "prepend" | "append" | "outerHTML" | "innerHTML"} Swap
+ */
+
+/**
+ * @param {Element} replacee
+ * @param {Element} replacer
+ * @param {Swap} swap
+ */
+function swap_elements(replacee, replacer, swap) {
 	if (swap === "delete") replacee.remove();
 	if (swap === "prepend") replacee.prepend(replacer);
 	if (swap === "append") replacee.append(replacer);
 	if (swap === "outerHTML") replacee.replaceWith(replacer);
 	if (swap === "innerHTML") replacee.replaceChildren(...replacer.childNodes);
-
-	// Reset the form
-	if (reset) form.reset();
-});
+}
 
 /**
-@param {HTMLFormElement} form
+@param {HTMLAnchorElement | HTMLFormElement} el
 @returns {{
   reset: boolean;
   target: string | undefined;
   select: string | undefined;
-  swap: "delete" | "prepend" | "append" | "outerHTML" | "innerHTML";
+  swap: Swap;
 	history: "push" | "replace" | "none";
 	delay: number;
 	confirm: string | undefined;
 }}
 */
 
-function config(form) {
-	let reset = form.hasAttribute("data-reset") ?? true;
-	let delay = Number(form.dataset.delay ?? 0);
+function config(el) {
+	let reset = el.hasAttribute("data-reset") ?? true;
+	let delay = Number(el.dataset.delay ?? 0);
 	if (Number.isNaN(delay)) {
-		throw new TypeError(`data-delay="${form.dataset.delay}" is not valid`);
+		throw new TypeError(`data-delay="${el.dataset.delay}" is not valid`);
 	}
 	let {
 		select,
@@ -67,7 +109,7 @@ function config(form) {
 		swap = "innerHTML",
 		history = "push",
 		confirm,
-	} = form.dataset;
+	} = el.dataset;
 	if (
 		swap !== "delete" &&
 		swap !== "prepend" &&
@@ -84,27 +126,38 @@ function config(form) {
 }
 
 /**
-@param {HTMLFormElement} form
+@param {HTMLAnchorElement | HTMLFormElement} el
 @param {HTMLElement | null} submitter
 @param {"push" | "replace" | "none"} history
 @returns {Promise<Response>}
 */
-function submit(form, submitter, history) {
-	let { action, method } = form;
-	if (
-		submitter instanceof HTMLInputElement ||
-		submitter instanceof HTMLButtonElement
-	) {
-		action ||= submitter.formAction;
-		method ||= submitter.formMethod;
+function request(el, submitter, history) {
+	let action = "";
+	let method;
+	if (el instanceof HTMLAnchorElement) {
+		action = el.href;
+		method = "get";
 	}
-	let headers = { "content-type": "application/x-www-form-urlencoded" };
-	let values = urlencoded(new FormData(form, submitter));
+	let headers;
+	let values = "";
+	if (el instanceof HTMLFormElement) {
+		action = el.action;
+		method = el.method;
+		if (
+			submitter instanceof HTMLInputElement ||
+			submitter instanceof HTMLButtonElement
+		) {
+			action ||= submitter.formAction;
+			method ||= submitter.formMethod;
+		}
+		headers = { "content-type": "application/x-www-form-urlencoded" };
+		values = urlencoded(new FormData(el, submitter)).toString();
+	}
 	/** @type {RequestInit} */
 	let options = { method, headers };
 	if (method === "get") {
 		let url = new URL(action);
-		url.search = values.toString();
+		url.search = values;
 		action = url.toString();
 		switch (history) {
 			case "push":
